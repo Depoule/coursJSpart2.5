5d39ab7996631b50da07e19a2c8a1d737b783b6c
Depoule <jpoulet02100@gmail.com>
2019-01-28 15:56:55 +0100

    exo 5 fini et corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+function imageAppear(img){
+document.getElementById(img).src='assets/images/' + img + '_2.jpg' ;
+}
+
+function changeFirstImage(){
+  document.getElementById('image1').src = 'assets/images/image1_2.jpg';
+}
+function changeSecondImage(){
+  document.getElementById('image2').src = 'assets/images/image2_2.jpg';
+}
+function changeThirdImage(){
+  document.getElementById('image3').src = 'assets/images/image3_2.jpg';
+}
+function changeFourthImage(){
+  document.getElementById('image4').src = 'assets/images/image4_2.jpg';
+}
+function changeFifthImage(){
+  document.getElementById('image5').src = 'assets/images/image5_2.jpg';
+}
